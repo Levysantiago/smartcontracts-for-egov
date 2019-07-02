f33c4e04812a46beb58c294f75489f9292503f36
Levysantiago <levyssantiago@gmail.com>
2019-07-01 22:03:17 -0300

    Adding more constants, from Student page

--- client/src/language/pt.js
+++ client/src/language/pt.js
@@ -1,4 +1,7 @@
 module.exports = {
+  /*PAGES TITLES*/
+  ENROLLMENT_PAGE_TITLE: "Comprovante de Matrícula",
+
   /* NAVBAR INFO */
   NAV_ADDRESS: "Endereço",
   NAV_CONTROLL: "Controle",
@@ -19,12 +22,32 @@ module.exports = {
   ENR_FRM_SCHEDULE_NIGHT: "Noturno",
   ENR_FRM_SCHEDULE_INTEGRAL: "Integral",
   ENR_FRM_METAMASK_ACCOUNT: "Conta Metamask",
+  ENR_FRM_METAMASK_STUDENT_ACCOUNT: "Conta Metamask do Estudante",
+  ENR_FRM_METAMASK_INSTITUTE_ACCOUNT: "Conta Metamask da Instituição",
+  ENR_FRM_METAMASK_CONTRACT_ACCOUNT: "Endereço do Contrato",
   ENR_FRM_BTN_ADD: "Adicionar",
   ENR_FRM_BTN_CANCEL: "Cancelar",
   ENR_FRM_BTN_EDIT: "Editar",
 
+  SUBJECT_FRM_CODE: "Código",
+  SUBJECT_FRM_NAME: "Nome da Matéria",
+  SUBJECT_FRM_SUBJECT: "Matéria",
+  SUBJECT_FRM_CLASS: "Turma",
+  SUBJECT_FRM_CREDIT: "Crédito",
+  SUBJECT_FRM_SCHEDULE: "Horário",
+  SUBJECT_FRM_ROOM: "Sala",
+  SUBJECT_FRM_METAMASK_ACCOUNT: "Endereço Metamask",
+  SUBJECT_FRM_SUBJECTS_LIST: "Lista de Matérias",
+
+  INPUT_STUDENT_ADDRESS: "Endereço do aluno",
+  INPUT_ALLOW_DISALLOW: "Permitir/Remover acesso",
+  INPUT_ADD_NEW_SUBJECT: "Adicionar nova matéria",
+
   /* ENROLLMENT LIST */
   CARD_LIST_TITLE: "Matrículas",
+  ENROLLMENT_CARD_TITLE: "Matrícula",
+  CARD_NO_PERMISSION_TITLE: "Sem permissão",
+  CARD_LOGIN_REQUIRED_TITLE: "Realize o Login",
 
   /* USER MESSAGES */
   MSG_ACCOUNT_VERIFICATION: "Verificando conta",
@@ -37,10 +60,29 @@ module.exports = {
   MSG_ENROLLMENT_ADDED: "Matrícula adicionada",
   MSG_NO_PAGE_PERMISSION:
     "Desculpa, esta conta não tem permissão para acessar a página.",
+  MSG_NO_ENROLLMENT_PERMISSION: "Sem permissão para essa matrícula",
+  MSG_NO_METAMASK_LOGIN:
+    "Por favor, acesse sua conta no Metamask para usar o sistema. Então atualize a página",
+  MSG_PERMISSION_VERIFICATION: "Verificando permissão",
+  MSG_ADDING_SUBJECT: "Adicionando nova matéria",
+  MSG_SUBJECT_ADDED: "Matéria adicionada!",
+  MSG_ALLOWING_ADDRESS: "Permitindo endereço",
+  MSG_ADDRESS_ALLOWED: "Endereço permitido!",
+  MSG_DISALLOWING_ADDRESS: "Removendo permissão de endereço",
+  MSG_ADDRESS_DISALLOWED: "Permissão removida!",
+  MSG_YOU_ARE_NOT_STUDENT:
+    "Você não é estudante. Tente pesquisar uma matrícula para verificar se tem acesso.",
 
   /* ERROR MESSAGES */
   ERR_VERIFYING_ACCOUNT: "Erro ao verificar conta",
   ERR_SENDING_DATA: "Erro ao enviar dados",
 
-  LANG_BUTTON_NAME: "en"
+  /* BUTTON NAMES */
+  LANG_BUTTON_NAME: "en",
+  REFRESH_BTN_NAME: "Atualizar página",
+  SEARCH_BTN_NAME: "Procurar",
+  SUBJECTS_MANAGEMENT_BTN_NAME: "Gerenciamento",
+  SUBJECT_ADD_BTN_NAME: "Adicionar",
+  ALLOW_BTN_NAME: "Permitir",
+  DISALLOW_BTN_NAME: "Remover"
 };
