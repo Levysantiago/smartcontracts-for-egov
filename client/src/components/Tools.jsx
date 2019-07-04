0b42b421e69ab7bcd11e743b89957fe9b5063209
Levysantiago <levyssantiago@gmail.com>
2019-07-04 00:42:52 -0300

    creating a component to the tools home page section

---
+++ client/src/components/Tools.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import ImgCard from "./ImgCard";
+
+class Tools extends Component {
+  render() {
+    const { images, linkColor } = this.props;
+    return (
+      <div>
+        {images.map(image => (
+          <ImgCard
+            key={image.id}
+            imgPath={require("../images/" + image.filename)}
+            link={image.link}
+            alt={image.alt}
+            linkColor={linkColor}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Tools;
