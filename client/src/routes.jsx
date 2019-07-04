e02060218c4a0df6424740693532af625cb2010d
Levysantiago <levyssantiago@gmail.com>
2019-07-04 00:39:44 -0300

    adding home page

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import App from "./pages/App";
+import Home from "./pages/Home";
 import Student from "./pages/Student";
 
 const Routes = (
   <Router>
-    <Route path="/" exact strict component={App} />
+    <Route path="/" exact strict component={Home} />
+    <Route path="/collegiate" exact strict component={App} />
     <Route path="/student" exact strict component={Student} />
   </Router>
 );
