e185d484413d66d83b7a4f30b14d74ef134407c4
Levysantiago <levyssantiago@gmail.com>
2019-07-04 00:37:56 -0300

    adapting the status div to homepage

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,26 +17,63 @@ class Navbar extends Component {
     });
   }
 
+  addressLabel(address, lang) {
+    if (address) {
+      return (
+        <label className="col s10 hide-on-med-and-down">
+          {lang.NAV_ADDRESS}: {address.toLowerCase()}
+        </label>
+      );
+    }
+  }
+
+  statusComponent(status) {
+    if (status) {
+      return (
+        <a
+          className={
+            "btn-floating btn-small z-depth-0 " + this.getStatusColor(status)
+          }
+          title={status}
+        >
+          {status.charAt(0)}
+        </a>
+      );
+    } else {
+      return (
+        <a
+          className={
+            "btn-floating btn-small z-depth-0 " + this.getStatusColor(status)
+          }
+          title={status}
+        />
+      );
+    }
+  }
+
   render() {
     const logoStyle = {
       marginTop: 10
     };
-    const { address, status, onLanguageChange, lang } = this.props;
+    const { address, onLanguageChange, lang, status } = this.props;
 
     return (
-      <nav>
+      <nav style={{ marginBottom: "10px" }}>
         <div className="nav-wrapper row white">
           <div className="brand-logo black-text row">
             <div className="col s2" style={logoStyle}>
               <MetamaskLogo width={50} height={40} move={false} id="2" />
             </div>
-            <label className="col s10 hide-on-med-and-down">
-              {lang.NAV_ADDRESS}: {address.toLowerCase()}
-            </label>
+            {this.addressLabel(address, lang)}
           </div>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
               <a className="black-text" href="/">
+                {lang.NAV_HOME}
+              </a>
+            </li>
+            <li>
+              <a className="black-text" href="/collegiate">
                 {lang.NAV_CONTROLL}
               </a>
             </li>
@@ -45,17 +82,7 @@ class Navbar extends Component {
                 {lang.NAV_ENROLLMENT}
               </a>
             </li>
-            <li>
-              <a
-                className={
-                  "btn-floating btn-small z-depth-0 " +
-                  this.getStatusColor(status)
-                }
-                title={status}
-              >
-                {status.charAt(0)}
-              </a>
-            </li>
+            <li>{this.statusComponent(status)}</li>
             <li>
               <a
                 className="waves-effect waves-light btn-small grey"
